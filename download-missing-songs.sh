03fad8c2685b75d772bb0ee61b793c1dc07e9d11
JaajoJ <71885933+JaajoJ@users.noreply.github.com>
2022-02-15 03:11:30 +0200

    Add files via upload

---
+++ download-missing-songs.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+cd $PWD
+cd songs/
+
+MISSINGSONGS=()
+URLS=()
+SONGFOUND=0
+#Making array of songs without mp3 and mp4 downloaded
+for d in *; do
+	cd "$d"
+	SONGFOUND=0
+
+	for f in *; do
+		echo "$f"
+		if [[ "${f: -4}" == ".mp3" ]] | [[ "${f: -4}" == ".mp4" ]]; then
+		SONGFOUND=1
+		fi
+	done;
+
+	if [[ "$SONGFOUND" == 0 ]]; then
+		MISSINGSONGS+=("$d")
+	fi
+	echo "download song? "$SONGFOUND
+	cd ..
+
+done;
+
+#Getting urls for the songs
+for value in "${MISSINGSONGS[@]}"
+do
+    	echo ""
+	echo "Give youtube url for:"
+	echo $value
+	read INPUT
+	URLS+=("$INPUT")
+done
+
+
+#Going one by one to folder and downloading needed songs
+COUNT=0
+for value in "${MISSINGSONGS[@]}"; do
+	cd "$value"
+	echo $PWD
+	echo "${URLS[$COUNT]}"
+	youtube-dl --format mp4 "${URLS[$COUNT]}"
+	ffmpeg -i *"${URLS[$COUNT]}".mp4 -vn -acodec libmp3lame -ac 2 -ab 160k -ar 48000 "${URLS[$COUNT]}".mp3
+	mv *.mp4 "${URLS[$COUNT]}".mp4
+	
+	#First remove then add the songs
+	grep -v '^#VIDEO:' *".txt" > temp && mv temp *".txt"
+	grep -v '^#MP3:' *".txt" > temp && mv temp *".txt"
+
+	echo '#MP3:'"${URLS[$COUNT]}"".mp3" | cat - *".txt" > temp && mv temp *".txt"
+	echo '#VIDEO:'"${URLS[$COUNT]}"".mp4" | cat - *".txt" > temp && mv temp *".txt"
+	COUNT=$COUNT+1
+	cd ..
+done
+
